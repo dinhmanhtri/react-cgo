0a50a75e43d108d4ca90b83d8bc74727df526148
dinhmanhtri <mtri02.it@gmail.com>
2022-06-23 23:27:20 +0700

    add alert component

---
+++ reactjs/react-components/alert-component/src/App.js
@@ -0,0 +1,9 @@
+import Alert from "./components/AlertComponent";
+
+function App() {
+  return (
+    <Alert text={"Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại."} />
+  );
+}
+
+export default App;
